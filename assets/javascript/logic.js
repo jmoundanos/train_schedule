ea0538b51d9fab4b9da853bffb891da5a7edf70c
Jennifer Moundanos <jmoundanos@gmail.com>
2020-01-22 18:45:09 -0600

    set layout and css

---
+++ assets/javascript/logic.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyBLDGWztbZq04dGCfXSN_bB41lsILojk-o",
+        authDomain: "train-schedule-f1def.firebaseapp.com",
+        databaseURL: "https://train-schedule-f1def.firebaseio.com",
+        projectId: "train-schedule-f1def",
+        storageBucket: "train-schedule-f1def.appspot.com",
+        messagingSenderId: "1036524729737",
+        appId: "1:1036524729737:web:14e88b1c0c69bb03e15d58"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+          
+  });
