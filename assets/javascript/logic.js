92098205a5830cc8b5fc0e37e275c51d03c82a3b
Jennifer Moundanos <jmoundanos@gmail.com>
2020-01-24 18:42:05 -0600

    finished basic logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,65 +8,60 @@ $(document).ready(function(){
         messagingSenderId: "1036524729737",
         appId: "1:1036524729737:web:14e88b1c0c69bb03e15d58"
       };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
     //Store database in variable for reference
     var database = firebase.database();
-    //initialize variables
-    var train_name = "";
-    var destination = "";
-    var first_train_time = "";
-    var frequency = 0;
-
-   //Capture button click
+    
+    //Capture button click
     $("button").on("click", function(event){
         event.preventDefault();
         //Get values from form input boxes
-        train_name = $("#train_name").val().trim();
-        destination = $("#destination").val().trim();
-        first_train_time = moment($("#first_train_time").val().trim(),"hh:mm").format("HH:mm");
-        frequency = $("#frequency").val().trim();
-        //console.log(first_train_time);
+        var train_name = $("#train_name").val().trim();
+        var destination = $("#destination").val().trim();
+        var first_train_time =  moment($("#first_train_time").val().trim(), "HH:mm").subtract(1, "years").format("X");
+        var frequency = $("#frequency").val().trim();
+        //get the current time
+        var currentTime = moment();
+   
         //Set values in database
         database.ref().push({
             train_name: train_name,
             destination: destination,
             first_train_time: first_train_time,
-            frequency: frequency
-        })
-        //console.log(train_name);
+            frequency: frequency,
+        });
+        //clear text boxes
+        $("#train_name").val("");
+        $("#destination").val("");
+        $("#first_train_time").val("");
+        $("#frequency").val("");
+
+        return false;
+    });   
         //Firebase watcher and initial loader
         database.ref().on("child_added", function(childSnapshot){
-            //Log what is coming from snapshot
-           // console.log(childSnapshot.val().train_name);
-            //console.log(childSnapshot.val().destination);
-            //console.log(childSnapshot.val().first_train_time);
-            //console.log(childSnapshot.val().frequency);
-            //Create variables to hold and calculate value of next arrival and minutes away
-            var firstTime = moment(first_train_time, "HH:mm").subtract(1,"years")
-            var currentTime = moment();
-            var diffTimes = currentTime.diff(moment(firstTime), "minutes");
-            var remainder = diffTimes % frequency;
-            var minutesAway = frequency - remainder;
-            var nextArrival = moment().add(minutesAway, "minutes");
-            //console.log(firstTime);
-            //console.log(currentTime);
-            console.log(diffTimes);
-            //console.log(remainder);
-            //console.log(minutesAway);
-            //var minutes_away = 0;
+           
+            //Store snapshots in variable
+            var firstTrain = childSnapshot.val().first_train_time;
+            var tFrequency = childSnapshot.val().frequency;
+            //Calculate remainder and store in variable    
+            var remainder = moment().diff(moment.unix(firstTrain),"minutes") % tFrequency;
+            //calculate minutes away and store in variable
+            var minutesAway = tFrequency - remainder;
+            //calculate the next arrival and store in variable
+            var arrivalTime = moment().add(minutesAway, "m").format("hh:mm A");
+        
             //Append data in database to table
             $("tbody").append(
                 "<tr><td>" + childSnapshot.val().train_name +
                 "</td><td>" + childSnapshot.val().destination +
                 "</td><td>" + childSnapshot.val().frequency +
-                "</td><td>" + childSnapshot.val().nextArrival +
-                "</td><td>" + childSnapshot.val().minutesAway +
+                "</td><td>" + arrivalTime +
+                "</td><td>" + minutesAway +
                 "</td></tr>"
             )
+        },function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
         })
-    })
-
-
-
-  });
+    });
